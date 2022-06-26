2c5509fc85d054dc8bebff56fbc8c666a1cbd8bd
Daniel Wagner <daniel.wagner@wglc.at>
2022-06-26 21:29:42 +0200

    t

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f23460f3ab15b49e"; }
+/******/ 		__webpack_require__.h = function() { return "70254fe9bbaeb8d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/welcomer.tsx
+++ components/welcomer.tsx
@@ -5,20 +5,20 @@ import image from '../public/profile.png'
 const Welcomer = () => {
     return (
     <div>
-        <div className="min-h-100vh clear-both flex align-center justify-center">
-            <div className="w-2/4 rounded-full px-12 flex content-center relative left-24">
-                <Image src={image} alt="Profile Picture" objectFit="cover" height="300px" width="300px"className="rounded-full" />
+        <div className="min-h-100vh clear-both flex align-center justify-center mt-28">
+            <div className="px-12 flex content-center relative right-36 rotate-45">
+                <Image src={image} alt="Profile Picture" objectFit="cover" height="300px" width="300px" className="rounded-[50px] hover:rounded-[60px] duration-100"/>
             </div>
-            <div className="w-2/4 flex flex-col md:ml-10 mx-10 mt-10 text-center">
+            <div className="flex flex-col md:ml-10 mx-10 mt-10 text-center">
                 <h1 className="font-bold text-7xl text-left mb-5">
-                    TestTEstTEst
+                    Daniel Wagner
                 </h1>
-                <p className="text-left font-normal mb-5 flex-wrap">
-                    Testetestt3est
+                <p className="text-lg text-left font-normal mb-5 flex-wrap">
+                    Mehr als siuuu kann ich leider noch nicht sagen :/
                 </p>
-                <Link href="/">
-                <a className="font-semibold text-white md:mt-10 mt-5 pt-5 bg-blue-500 rounded-md w-60 h-16 text-lg hover:bg-black duration-300 transition-colors">
-                    Testtest
+                <Link href="/projects">
+                <a className="font-semibold text-white md:mt-10 mt-5 pt-5 bg-blue-500 rounded-md w-60 h-16 text-lg hover:bg-gray-700 hover:-translate-y-1 hover:scale-105 duration-200 ease-in-out transition">
+                    Meine Projekte
                 </a>
                 </Link>
             </div>
