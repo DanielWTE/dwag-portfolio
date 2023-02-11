23a67c6cd4b80c4ecaab2f1ba587ece85b9e77f4
Daniel Wagner <dw@wglc.at>
2023-02-11 23:20:04 +0100

    Updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf9dd79e394de957"; }
+/******/ 		__webpack_require__.h = function() { return "6fa7e5f24473c583"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
